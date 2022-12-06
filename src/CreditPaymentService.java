016efbd9db604b4df2d63c635e1592816279a862
Menshikov.SI <89089118626@mail.ru>
2022-12-06 09:08:15 +0500

    delet if

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,15 +1,6 @@
 public class CreditPaymentService {
     public int calculate(int timeCredit, int sumCredit) {
 
-        if (timeCredit == 1) {
-            timeCredit = 12;
-        }
-        if (timeCredit == 2) {
-            timeCredit = 24;
-        }
-        if (timeCredit == 3) {
-            timeCredit = 36;
-        }
         double exponentiation = (1 + (9.99 / (12 * 100)));
         int percent = (int) ((sumCredit * (9.99 / (12 * 100))) / (1 - (Math.pow(exponentiation, -timeCredit))));
 
--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
 
         int sum = 1_000_000;
-        int time12 = 1;
+        int time12 = 12;
         int payment = service.calculate(time12, sum);
         System.out.println("Ежемесячный платеж при сроке кредитования 1 год - " + payment);
 
-        int time24 = 2;
+        int time24 = 24;
         int payment24 = service.calculate(time24, sum);
         System.out.println("Ежемесячный платеж при сроке кредитования 2 года - " + payment24);
 
-        int time36 = 3;
+        int time36 = 36;
         int payment36 = service.calculate(time36, sum);
         System.out.println("Ежемесячный платеж при сроке кредитования 3 года - " + payment36);
     }
