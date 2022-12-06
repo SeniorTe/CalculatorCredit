c40d5caa5e3098a3fdd90a99d03d70da32394022
Menshikov.SI <89089118626@mail.ru>
2022-12-06 16:13:41 +0500

    term in years

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,7 +2,7 @@ public class CreditPaymentService {
     public int calculate(int timeCredit, int sumCredit) {
 
         double exponentiation = (1 + (9.99 / (12 * 100)));
-        int percent = (int) ((sumCredit * (9.99 / (12 * 100))) / (1 - (Math.pow(exponentiation, -timeCredit))));
+        int percent = (int) ((sumCredit * (9.99 / (12 * 100))) / (1 - (Math.pow(exponentiation, -(timeCredit * 12)))));
 
         return percent;
     }
